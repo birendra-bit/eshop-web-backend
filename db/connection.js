703972aa7fba6844a46069fe7c2e11af1bc65c4f
Birendra Bhujel <birendra.bhujel@cybergindia.local>
2020-05-21 17:37:40 +0530

    connection string made with online db

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,8 @@ const {MONGO_HOSTNAME,MONGO_PORT, MONGO_DB} = require('./keys')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const url = `mongodb://${process.env.MONGO_HOSTNAME || MONGO_HOSTNAME}:${process.env.MONGO_PORT || MONGO_PORT}/${process.env.MONGO_DB || MONGO_DB}`;
-
+// const url = `mongodb://${process.env.MONGO_HOSTNAME || MONGO_HOSTNAME}:${process.env.MONGO_PORT || MONGO_PORT}/${process.env.MONGO_DB || MONGO_DB}`;
+const url = 'mongodb+srv://birendra:mongouser@cluster0-qurbt.mongodb.net/test?retryWrites=true&w=majority'
 // console.log(process.env.MONGO_PORT);
 mongoose.connect(url,{useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true})
 
